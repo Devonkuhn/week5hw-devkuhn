2c56cc96271f85e4d50627378dc19ba8db81b7d6
kuhndev <86081460+immortalized513@users.noreply.github.com>
2021-09-07 17:07:46 -0400

    updated js
    added time entries for the workday timeline

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,3 +7,14 @@ var currentHour = moment().format("h A")
 
 $("#currentDay").text(currentTime);
 
+var workdayTimeline = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+];
